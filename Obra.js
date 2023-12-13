3c473b67ba3dc82417be48db76e6c79e69e3fc76
el marica de yael <yaelop117@gmail.com>
2023-12-13 15:32:08 -0600

    primer commit

---
+++ Obra.js
@@ -0,0 +1,24 @@
+
+class obra{
+
+obra(){
+
+}
+
+localizacion_replanteo(trabajadores) {
+ var dias ;
+ dias =trabajadores/2;
+ return dias; 
+
+    
+}
+
+
+
+
+
+
+}
+var work =new obra();
+var dia =work.localizacion_replanteo(4);
+console.log(dia);
